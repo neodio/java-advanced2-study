e11c867d13862b5aaf73eb26484a63cebd921efd
장정우 <jungwoo@lotte.net>
2025-04-18 16:32:43 +0900

    20250418 실전 자바 - 고급 2편, I/O, 네트워크, 리플렉션 - 애노테이션

---
+++ src/annotation/basic/AnnoElement.java
@@ -0,0 +1,15 @@
+package annotation.basic;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import util.MyLogger;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AnnoElement {
+    String value();
+    int count() default 0;
+    String[] tags() default {};
+
+    //MyLogger data(); // 다른 타입은 적용X
+    Class<? extends MyLogger> annoData() default MyLogger.class;
+}
